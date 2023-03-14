0c10a3748d943b5c35464d3a55da059d76976b16
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-14 17:14:52 +0300

    = rework sign_in

--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -1,15 +1,23 @@
 from django.contrib.auth import authenticate, login
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+from api.serializers import UserSerializer
+from core.models import User
+
 
 class AuthViewSet(viewsets.GenericViewSet):
-    permission_classes = AllowAny
+    permission_classes = (AllowAny,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def create(self, request):
-        email = request.data.get('email', None)
-        password = request.data.get('password', None)
+    @action(detail=False, methods=['post'])
+    def sign_in(self, request):
+        serializer = self.get_serializer(data=request.data)
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
 
         if email is None or password is None:
             return Response({'Message': "Missing required params"}, status=status.HTTP_400_BAD_REQUEST)
