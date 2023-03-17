512a4a10e0316106cc03aec9ee6d22ada7667b1e
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-17 14:11:25 +0300

    + yasg

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,14 @@
+from django.urls import path
 from rest_framework import routers
 
-from api.views import ChatViewSet
+from api.openapi import schema_view
+from api.views import ChatViewSet, AuthViewSet
 
 router = routers.DefaultRouter()
 router.register(r'chats', ChatViewSet)
+router.register(r'auth', AuthViewSet)
 
 urlpatterns = router.urls
+urlpatterns += [
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger")
+]
