f9d308179acafc3d561d7cc80a439d3add9b719e
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-17 12:29:33 +0300

    ! AuthSignUpSerializer .validate & .create

--- src/api/serializers/user.py
+++ src/api/serializers/user.py
@@ -3,19 +3,21 @@ from rest_framework import serializers
 from core.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class AuthSignUpSerializer(serializers.ModelSerializer):
     def create(self, data):
-        user = User.objects.create_user(
-            email=data['email'],
-            password=data['password'],
-        )
-
+        user = User.objects.create_user(email=data['email'])
+        user.set_password(data['password'])
         return user
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_repeat']:
+            raise serializers.ValidationError("Passwords are not similar")
 
     class Meta:
         model = User
         fields = ['pk', 'email', 'password', 'repeat_password']
         extra_kwargs = {
-            'password': {'write_only': True},
-            'password_repeat': {'write_only': True},
+            'password': {'write_only': True, "required": True},
+            'password_repeat': {'write_only': True, "required": True},
+            'email': {"required": True}
         }
