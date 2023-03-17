cc12a0cda5a3f04a995d45d621a6162214b15a2d
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-17 12:30:03 +0300

    ! def sign_up & - get_query_set

--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -3,34 +3,22 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from api.serializers.user import UserSerializer
+from api.serializers.user import AuthSignUpSerializer
 from core.models import User
 
 
 class AuthViewSet(viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
     queryset = User.objects.all()
-    serializer_class = UserSerializer
 
-    @action(details=False, methods=['post'])
+    @action(details=False, methods=['POST'])
     def sign_up(self, request):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid()
-
-        if self.get_queryset():
-            return Response({'Message': "Email already exist"}, status=status.HTTP_400_BAD_REQUEST)
-        if serializer.validated_data['password'] != serializer.validated_data['password_repeat']:
-            return Response({'Message': "Passwords are not similar"}, status=status.HTTP_400_BAD_REQUEST)
-        if serializer.validated_data['email'] is None or\
-                serializer.validated_data['password'] is None or\
-                serializer.validated_data['password_repeat'] is None:
-            return Response({'Message': "Missing required args"}, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
 
         return Response(user, status=status.HTTP_201_CREATED)
 
-    def filter_queryset(self, queryset: QuerySet) -> QuerySet:
-        if self.action == 'sign_up':
-            email = self.request.data.get('email')
-            return super().filter_queryset(queryset).get(email=email)
+    def get_serializer_class(self):
+        if self.action == "sign_up":
+            return AuthSignUpSerializer()
