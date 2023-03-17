d36bd1930966f8493f77b2a2299b1fdd3ac4a239
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-17 14:33:22 +0300

    + swagger example serializator

--- src/api/serializers/user.py
+++ src/api/serializers/user.py
@@ -3,6 +3,19 @@ from rest_framework import serializers
 from core.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "pk",
+            "username",
+            "is_active",
+        )
+        extra_kwargs = {
+            "is_active": {"read_only": True},
+        }
+
+
 class AuthSignUpSerializer(serializers.ModelSerializer):
     password_repeat = serializers.CharField(write_only=True, min_length=8, max_length=128,
                                             required=True, trim_whitespace=True)
@@ -10,6 +23,8 @@ class AuthSignUpSerializer(serializers.ModelSerializer):
     def create(self, data):
         user = User.objects.create_user(email=data['email'])
         user.set_password(data['password'])
+        user.save()
+
         return user
 
     def validate(self, attrs):
@@ -25,4 +40,13 @@ class AuthSignUpSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True, "required": True, "min_length": 8},
             'email': {"required": True}
+        }
+
+
+class AuthUserOutputSerializer(serializers.ModelSerializer):
+
+    class Meta(UserSerializer.Meta):
+        fields = ["email"]
+        extra_kwargs = {
+            'email': {"read_only": True}
         }
--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -1,11 +1,12 @@
 from django.db.models import QuerySet
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.settings import api_settings
 
-from api.serializers.user import AuthSignUpSerializer
+from api.serializers.user import AuthSignUpSerializer, AuthUserOutputSerializer
 from core.models import User
 
 
@@ -13,6 +14,11 @@ class AuthViewSet(viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
     queryset = User.objects.all()
 
+    @swagger_auto_schema(
+        request_body=AuthSignUpSerializer,
+        responses={status.HTTP_201_CREATED: AuthUserOutputSerializer()},
+        operation_summary="Create new account",
+    )
     @action(detail=False, methods=['POST'])
     def sign_up(self, request):
         serializer = self.get_serializer(data=request.data)
