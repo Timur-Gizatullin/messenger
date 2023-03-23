1af38438052fe55138223c0fc6358d1acaa81c12
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-23 15:48:52 +0300

    + DestroyModelMixin

--- src/api/views/message.py
+++ src/api/views/message.py
@@ -1,22 +1,21 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Q
 from rest_framework.mixins import DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
-from api.serializers.message import MessageDeleteSerializer
-from core.models import Chat
+from core.models import Message
 
 
 class MessageViewSet(DestroyModelMixin, GenericViewSet):
     permission_classes = (IsAuthenticated,)
-    queryset = Chat.objects.all()
+    queryset = Message.objects.all()
 
     def filter_queryset(self, queryset: QuerySet) -> QuerySet:
         if self.action == "destroy":
-            return super().filter_queryset(queryset).filter(author=self.request.user)
+            user = self.request.user
+            return (
+                super().filter_queryset(queryset)
+                .filter(Q(author=user) | Q(forwarded_by=user))
+            )
         else:
             return super().filter_queryset(queryset)
-
-    def get_serializer_class(self):
-        if self.action == 'destroy':
-            return MessageDeleteSerializer
