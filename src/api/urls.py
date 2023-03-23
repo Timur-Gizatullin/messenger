9021564ce5cdf094b6bdfb4dd4cd37e4af66c538
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-23 12:39:25 +0300

    ! rework logic

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,14 +4,10 @@ from rest_framework import routers
 from api.openapi import schema_view
 from api.views import ChatViewSet
 from api.views.auth import AuthViewSet
-from api.views.message import MessageViewSet
 
 router = routers.DefaultRouter()
 router.register("chats", ChatViewSet)
 router.register("auth", AuthViewSet)
-router.register("messages", MessageViewSet, basename="message")
 
 urlpatterns = router.urls
-urlpatterns += [path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger")]
-
 urlpatterns += [path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger")]
--- src/api/views/chat.py
+++ src/api/views/chat.py
@@ -1,19 +1,43 @@
 from django.db.models import QuerySet
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+
 from rest_framework.viewsets import GenericViewSet
 
 from api.serializers.chat import ChatCreateSerializer, ChatSerializer
-from core.models import Chat
+from api.serializers.message import MessageSerializer
+from core.models import Chat, Message
 
 
 class ChatViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Chat.objects.all()
+    pagination_class = LimitOffsetPagination
+
+    limit = openapi.Parameter('limit', openapi.IN_QUERY, description="Number of results to return per page.", type=openapi.TYPE_INTEGER)
+    offset = openapi.Parameter('offset', openapi.IN_QUERY, description="The initial index from which to return the results.", type=openapi.TYPE_INTEGER)
+
+    @swagger_auto_schema(manual_parameters=[limit, offset])
+    @action(detail=True, methods=["GET"], url_path="messages")
+    def get_messages(self, request, pk):
+        return self.list(request)
+
+    def get_queryset(self):
+        if self.action == "get_messages":
+            return Message.objects.all()
+        else:
+            return Chat.objects.all()
 
     def filter_queryset(self, queryset: QuerySet) -> QuerySet:
         if self.action == "list":
             return super().filter_queryset(queryset).filter(users=self.request.user)
+        elif self.action == "get_messages":
+            return super().filter_queryset(queryset).filter(chat=self.kwargs["pk"])
         else:
             return super().filter_queryset(queryset)
 
@@ -22,3 +46,5 @@ class ChatViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
             return ChatSerializer
         elif self.action == "create":
             return ChatCreateSerializer
+        elif self.action == "get_messages":
+            return MessageSerializer
