aab6235526ed99bdb3aa662006a9b279eab85b25
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-27 10:51:14 +0300

    ! rename test

--- src/tests/users/test_find_user.py
+++ src/tests/users/test_find_user.py
@@ -8,7 +8,7 @@ from tests.factories.user import UserFactory
 
 @pytest.mark.parametrize(("user_email", "expected_len"), [("qw", 3), ("qwer", 3), ("qwertla1", 1), ("", 45)])
 @pytest.mark.django_db
-def test__get_messages_with_pagination__when_user_is_a_chat_member(user_email, expected_len, api_client):
+def test__find_user__success_case(user_email, expected_len, api_client):
     users = UserFactory.create_batch(42)
     users_to_find = UserFactory.create_batch(3)
     for i in range(len(users_to_find)):
@@ -17,8 +17,6 @@ def test__get_messages_with_pagination__when_user_is_a_chat_member(user_email, e
 
     api_client.force_authenticate(user=users[0])
     response = api_client.get(reverse("user-find-user"), {"email": user_email})
-
-    uuu = User.objects.all()
 
     assert response.status_code == status.HTTP_200_OK
     assert len(response.data) == expected_len
