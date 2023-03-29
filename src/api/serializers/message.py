1bad531b170b1aa6fd1f89c3ad83c36751284e4d
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-29 14:29:07 +0300

    + tests

--- src/api/serializers/message.py
+++ src/api/serializers/message.py
@@ -17,15 +17,10 @@ class MessageCreateSerializer(serializers.ModelSerializer):
 
         if not chat_queryset.filter(users__id=author.pk):
             raise serializers.ValidationError("Impossible to send message to the chat")
-        elif chat_queryset.filter(is_dialog=True).filter(users__is_deleted=True):
+        if chat_queryset.filter(is_dialog=True).filter(users__is_deleted=True):
             raise serializers.ValidationError("A member is deleted")
-        elif text is None or text.strip() == "":
-            raise serializers.ValidationError("text or picture should be filled")
-        elif replied_to:
-            message_replied_to = get_object_or_404(Message, pk=replied_to["pk"])
-            attrs["replied_to"] = message_replied_to
-            if message_replied_to.chat != chat_id:
-                raise serializers.ValidationError("Can not reply to this chat")
+        if replied_to and replied_to.chat.pk != int(chat_id):
+            raise serializers.ValidationError("Can't reach the message")
 
         attrs["author"] = author
         attrs["chat"] = chat_queryset.get()
@@ -43,6 +38,7 @@ class MessageCreateSerializer(serializers.ModelSerializer):
             "chat": {"read_only": True},
             "author": {"read_only": True},
             "forwarded_by": {"read_only": True},
+            "text": {"required": True}
         }
 
 
--- src/tests/chats/test_unit/test_add_message.py
+++ src/tests/chats/test_unit/test_add_message.py
@@ -1,4 +1,5 @@
 import pytest
+
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -8,11 +9,66 @@ from tests.factories.user import UserFactory
 
 
 @pytest.mark.django_db
+def test__add_message__when_text_filled(api_client):
+    user = UserFactory()
+    chat = ChatFactory(users=[user])
+    payload = {"text": "any message", }
+
+    api_client.force_authenticate(user=user)
+    response = api_client.post(reverse("chat-add-message", args=[chat.pk]), data=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data["text"] == payload["text"]
+    assert response.data["author"] == user.pk
+    assert response.data["chat"] == chat.pk
+
+
+@pytest.mark.django_db
+def test__add_message__when_text_and_replied_to_filled(api_client):
+    user = UserFactory()
+    chat = ChatFactory(users=[user])
+    message = MessageFactory(author=UserFactory(), chat=chat)
+    payload = {"replied_to": message.pk, "text": "any message", }
+
+    api_client.force_authenticate(user=user)
+    response = api_client.post(reverse("chat-add-message", args=[chat.pk]), data=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data["text"] == payload["text"]
+    assert response.data["author"] == user.pk
+    assert response.data["chat"] == chat.pk
+    assert response.data["replied_to"] == message.pk
+
+
+@pytest.mark.django_db
 def test__add_message__when_author_is_not_chat_member(api_client):
     user = UserFactory()
     chat = ChatFactory()
+    payload = {"text": "any message", }
 
     api_client.force_authenticate(user=user)
-    response = api_client.post(reverse("chat-add-message", args=[chat.pk]), data={"text": "any message", })
+    response = api_client.post(reverse("chat-add-message", args=[chat.pk]), data=payload)
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test__add_message__when_text_not_filled(api_client):
+    user = UserFactory()
+    chat = ChatFactory(users=[user])
+    payload = {}
+
+    api_client.force_authenticate(user=user)
+    response = api_client.post(reverse("chat-add-message", args=[chat.pk]), data=payload)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test__add_message__when_not_auth(api_client):
+    chat = ChatFactory()
+    payload = {"text": "any message", }
+
+    response = api_client.post(reverse("chat-add-message", args=[chat.pk]), data=payload)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
