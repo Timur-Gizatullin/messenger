a5349038872be2696df1fd283eaae64efb1db3cf
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-04-05 15:26:16 +0300

    + output type hints on stdiamgeserializer

--- src/api/serializers/image.py
+++ src/api/serializers/image.py
@@ -8,20 +8,20 @@ class StdImageSerializer(serializers.Serializer):
     medium = serializers.SerializerMethodField()
     large = serializers.SerializerMethodField()
 
-    def get_original(self, picture: StdImageFieldFile):
+    def get_original(self, picture: StdImageFieldFile) -> str:
         return picture.url
 
-    def get_thumbnail(self, picture: StdImageFieldFile):
+    def get_thumbnail(self, picture: StdImageFieldFile) -> str | None:
         if hasattr(picture, "thumbnail"):
             return picture.thumbnail.url
         return None
 
-    def get_medium(self, picture: StdImageFieldFile):
+    def get_medium(self, picture: StdImageFieldFile) -> str | None:
         if hasattr(picture, "medium"):
             return picture.medium.url
         return None
 
-    def get_large(self, picture: StdImageFieldFile):
+    def get_large(self, picture: StdImageFieldFile) -> str | None:
         if hasattr(picture, "large"):
             return picture.large.url
         return None
