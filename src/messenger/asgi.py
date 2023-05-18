4eb991fbe23d5b3cf662f0b9e39dce1e5c551c16
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-05-18 16:40:59 +0300

    + flake noqa

--- src/messenger/asgi.py
+++ src/messenger/asgi.py
@@ -2,17 +2,16 @@ import os
 
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messenger.settings")
-django.setup()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.sessions import CookieMiddleware
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messenger.settings")
+django.setup()
 django_asgi_app = get_asgi_application()
 
-from api.urls import websocket_urlpatterns
-from core.middlewares.cookie_auth_middleware import CookieAuthTokenMiddleware
+from api.urls import websocket_urlpatterns  # noqa: E402
+from core.middlewares.cookie_auth_middleware import CookieAuthTokenMiddleware  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
